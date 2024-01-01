f0e3d92d35f3de919e124ba997d8c3b021b40b1e
JuliusUlee <61336116+Julius-Ulee@users.noreply.github.com>
2024-01-01 08:09:58 +0700

    :shipit: Daily-Weather

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -51,12 +51,55 @@ sunset_readable=$(date -d @$sunset_unix +'%Y-%m-%d %H:%M:%S')
 timezone=$(echo "$weather_info" | jq -r '.timezone')
 coord_lon=$(echo "$weather_info" | jq -r '.coord.lon')
 coord_lat=$(echo "$weather_info" | jq -r '.coord.lat')
+wind_direction=$(echo "$weather_info" | jq -r '.wind.deg')
+wind_direction_text() {
+    local degree=$1
 
-echo "# <h1 align='center'><img height='40' src='images/cloud.png'> Daily Weather Report <img height='40' src='images/cloud.png'></h1>" > README.md
+    if (( $(echo "$degree >= 348.75" | bc -l) || $(echo "$degree < 11.25" | bc -l) )); then
+        echo "North"
+    elif (( $(echo "$degree >= 11.25" | bc -l) && $(echo "$degree < 33.75" | bc -l) )); then
+        echo "North-Northeast"
+    elif (( $(echo "$degree >= 33.75" | bc -l) && $(echo "$degree < 56.25" | bc -l) )); then
+        echo "Northeast"
+    elif (( $(echo "$degree >= 56.25" | bc -l) && $(echo "$degree < 78.75" | bc -l) )); then
+        echo "East-Northeast"
+    elif (( $(echo "$degree >= 78.75" | bc -l) && $(echo "$degree < 101.25" | bc -l) )); then
+        echo "East"
+    elif (( $(echo "$degree >= 101.25" | bc -l) && $(echo "$degree < 123.75" | bc -l) )); then
+        echo "East-Southeast"
+    elif (( $(echo "$degree >= 123.75" | bc -l) && $(echo "$degree < 146.25" | bc -l) )); then
+        echo "Southeast"
+    elif (( $(echo "$degree >= 146.25" | bc -l) && $(echo "$degree < 168.75" | bc -l) )); then
+        echo "South-Southeast"
+    elif (( $(echo "$degree >= 168.75" | bc -l) && $(echo "$degree < 191.25" | bc -l) )); then
+        echo "South"
+    elif (( $(echo "$degree >= 191.25" | bc -l) && $(echo "$degree < 213.75" | bc -l) )); then
+        echo "South-Southwest"
+    elif (( $(echo "$degree >= 213.75" | bc -l) && $(echo "$degree < 236.25" | bc -l) )); then
+        echo "Southwest"
+    elif (( $(echo "$degree >= 236.25" | bc -l) && $(echo "$degree < 258.75" | bc -l) )); then
+        echo "West-Southwest"
+    elif (( $(echo "$degree >= 258.75" | bc -l) && $(echo "$degree < 281.25" | bc -l) )); then
+        echo "West"
+    elif (( $(echo "$degree >= 281.25" | bc -l) && $(echo "$degree < 303.75" | bc -l) )); then
+        echo "West-Northwest"
+    elif (( $(echo "$degree >= 303.75" | bc -l) && $(echo "$degree < 326.25" | bc -l) )); then
+        echo "Northwest"
+    elif (( $(echo "$degree >= 326.25" | bc -l) && $(echo "$degree < 348.75" | bc -l) )); then
+        echo "North-Northwest"
+    else
+        echo "Unknown"
+    fi
+}
+wind_direction_text=$(wind_direction_text $wind_direction)
+weather_description_with_wind="Wind Direction: ${wind_direction_text:-Unknown}"
+forecast_24h_info=$(curl -s "http://api.openweathermap.org/data/2.5/forecast?q=${city_encoded}&cnt=8&appid=${OPENWEATHERMAP_API_KEY}")
+
+echo "# <h1 align='center'><img height='35' src='images/cloud.png'> Daily Weather Report <img height='35' src='images/cloud.png'></h1>" > README.md
 echo -e "<h3 align='center'>🕒 Indonesian Time(UTC$(printf "%+.2f" "$(bc <<< "scale=2; $timezone / 3600")")): <u>$time</u> (🤖Automated)</h3>\n" >> README.md
 echo -e "<table align='center'>" >> README.md
 echo -e "<tr>" >> README.md
-echo -e "<td align='center'><img src='images/placeholder.png' height='18'> <b>${city}</b><br><b>Latitude: ${coord_lat:-0} Longitude: ${coord_lon:-0}</b><br><img src='images/thermometer.png' height='18'> <b>${temperature_celsius:-0}°C</b><br><img src='${icon_url}' height='50'><br><b>$condition</b><br><b>($condition1)</b><br><b>Feels Like: ${feels_like_celsius:-0}°C</b></td>" >> README.md
+echo -e "<td align='center'><img src='images/placeholder.png' height='18'> <b>${city}</b><br><b>Latitude: ${coord_lat:-0} Longitude: ${coord_lon:-0}</b><br><img src='images/thermometer.png' height='18'> <b>${temperature_celsius:-0}°C</b><br><img src='${icon_url}' height='50'><br><b>$condition</b><br><b>($condition1)</b><br><b>Feels Like: ${feels_like_celsius:-0}°C<br><b>${weather_description_with_wind}</b></b></td>" >> README.md
 echo -e "</tr>" >> README.md
 echo -e "<td>" >> README.md
 echo -e "<table>" >> README.md
@@ -84,6 +127,31 @@ echo -e "<td align='center'><img src='images/sunrise.png' height='25'><br>Sunris
 echo -e "<td align='center'><img src='images/sunsets.png' height='25'><br>Sunset:<br><b>${sunset_readable:-0}</b></td>" >> README.md
 echo -e "</tr>" >> README.md
 echo -e "</table>" >> README.md
+echo -e "</table>" >> README.md
+echo "<h2 align="center"><img src="images/clock.png" height="25"> Next 24 Hours</h2>" >> README.md
+
+current_time=$(date +'%Y-%m-%d %H:%M:%S')
+twenty_four_hours_later=$(date -d "+24 hours" +'%Y-%m-%d %H:%M:%S')
+echo -e "<table align="center">" >> README.md
+echo -e "<tr>" >> README.md
+
+for ((i=0; i<8; i++)); do
+    forecast_date_unix=$(echo "$forecast_24h_info" | jq -r ".list[$i].dt")
+    forecast_date_readable=$(date -d @$forecast_date_unix +'%Y-%m-%d %H:%M:%S')
+
+    if [[ $forecast_date_readable > $current_time && $forecast_date_readable < $twenty_four_hours_later ]]; then
+        forecast_condition=$(echo "$forecast_24h_info" | jq -r ".list[$i].weather[0].main")
+        forecast_temperature_kelvin=$(echo "$forecast_24h_info" | jq -r ".list[$i].main.temp")
+        forecast_temperature_celsius=$(kelvin_to_celsius $forecast_temperature_kelvin)
+        weather_icon_code=$(echo "$forecast_24h_info" | jq -r ".list[$i].weather[0].icon")
+
+        icon_url="https://openweathermap.org/img/w/${weather_icon_code}.png"
+
+        echo -e "<td align="center"><b>${forecast_temperature_celsius:-0}°C</b><br><img src='$icon_url' height='50'><br><b>$forecast_condition</b><br><b>${forecast_date_readable:11:5}</b></td>" >> README.md        
+    fi
+done
+
+echo -e "</tr>" >> README.md
 echo -e "</table>" >> README.md
 
 git config --global user.email "action@github.com"
