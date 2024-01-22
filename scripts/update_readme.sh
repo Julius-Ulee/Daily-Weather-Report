570fda57968726f271ef85cf968858bb3561019d
JuliusUlee <61336116+Julius-Ulee@users.noreply.github.com>
2024-01-22 08:20:57 +0700

    :shipit: Daily-Weather v1.1.0

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -37,6 +37,8 @@ gust_speed=$(echo "$weather_info" | jq -r '.wind.gust // empty')
 wind_speed=$(echo $weather_info | jq -r '.wind.speed // empty')
 rainfall=$(echo "$weather_info" | jq -r '.rain."1h"')
 rainfall_mm=$(echo "scale=2; $rainfall * 25.4" | bc)
+snowfall=$(echo "$weather_info" | jq -r '.snow."1h" // empty')
+snowfall_mm=$(echo "scale=2; $snowfall * 25.4" | bc)
 
 temp_min_celsius=$(kelvin_to_celsius ${temp_min_kelvin:-0})
 temp_max_celsius=$(kelvin_to_celsius ${temp_max_kelvin:-0})
@@ -106,7 +108,8 @@ echo -e "</tr>" >> README.md
 echo -e "<td>" >> README.md
 echo -e "<table>" >> README.md
 echo -e "<tr>" >> README.md
-echo -e "<td align="center" colspan="2"><img src="images/rain.png" height="25"><br>Rainfall: <b>${rainfall_mm:-0} Millimeters</b></td>" >> README.md
+echo -e "<td align='center'><img src='images/rain.png' height="25"><br>Rainfall: <br><b>${rainfall_mm:-0} Millimeters</b></td>" >> README.md
+echo -e "<td align='center'><img src='images/snow.png' height='25'><br>Snowfall: <br><b>${snowfall_mm:-0} Millimeters</b></td>" >> README.md
 echo -e "</tr>" >> README.md
 echo -e "<tr>" >> README.md
 echo -e "<td align='center'><img src='images/fast.png' height='25'><br>Minimum<br>Temperature:<br><b>${temp_min_celsius:-0}°C</b></td>" >> README.md
@@ -165,5 +168,90 @@ git config --global user.name "GitHub Action"
 git add README.md
 git commit -m "🐙Update README with dynamic content `date +'%Y-%m-%d %H:%M:%S %Z'`"
 git pull origin main
+git push origin main
 
+#!/bin/bash
+
+echo "Time,City,Latitude,Longitude,Temperature_Celsius,Condition,Condition_Description,Icon_Code,Temp_Min_Celsius,Temp_Max_Celsius,Humidity,Feels_Like_Celsius,Pressure,Visibility,Wind_Deg,Gust_Speed,Wind_Speed,Rainfall_mm,Snowfall_mm,Clouds,Sunrise_Readable,Sunset_Readable,Timezone,Coord_Lon,Coord_Lat,Wind_Direction_Text,Weather_Description_With_Wind" > temp.csv
+
+echo "${time},${city},${latitude},${longitude},${temperature_celsius},${condition},${condition1},${weather_icon},${temp_min_celsius},${temp_max_celsius},${humidity},${feels_like_celsius},${pressure},${visibility},${wind_deg},${gust_speed},${wind_speed},${rainfall_mm},${snowfall_mm},${clouds},${sunrise_readable},${sunset_readable},${timezone},${coord_lon},${coord_lat},${wind_direction_text},${weather_description_with_wind}" >> temp.csv
+tail -n +2 weather_history.csv >> temp.csv
+
+mv temp.csv weather_history.csv
+
+git add weather_history.csv
+git commit -m "📄 Update weather history data $(date +'%Y-%m-%d %H:%M:%S %Z')"
+git pull origin main
+git push origin main
+
+#!/bin/bash
+
+json_data=$(cat <<EOF
+{
+  "city": "${city}",
+  "latitude": ${latitude},
+  "longitude": ${longitude},
+  "temperature_celsius": ${temperature_celsius},
+  "condition": "${condition}",
+  "condition_description": "${condition1}",
+  "icon_code": "${weather_icon}",
+  "temp_min_celsius": ${temp_min_celsius},
+  "temp_max_celsius": ${temp_max_celsius},
+  "humidity": ${humidity},
+  "feels_like_celsius": ${feels_like_celsius},
+  "pressure": ${pressure},
+  "visibility": ${visibility},
+  "wind_deg": ${wind_deg},
+  "gust_speed": ${gust_speed},
+  "wind_speed": ${wind_speed},
+  "rainfall_mm": ${rainfall_mm},
+  "snowfall_mm": ${snowfall_mm},
+  "clouds": ${clouds},
+  "sunrise_readable": "${sunrise_readable}",
+  "sunset_readable": "${sunset_readable}",
+  "timezone": ${timezone},
+  "coord_lon": ${coord_lon},
+  "coord_lat": ${coord_lat},
+  "wind_direction_text": "${wind_direction_text}",
+  "weather_description_with_wind": "${weather_description_with_wind}",
+  "forecast_24h": [
+EOF
+)
+
+for ((i=0; i<8; i++)); do
+    forecast_date_unix=$(echo "$forecast_24h_info" | jq -r ".list[$i].dt")
+    forecast_date_readable=$(date -d @$forecast_date_unix +'%Y-%m-%d %H:%M:%S')
+
+    if [[ $forecast_date_readable > $current_time && $forecast_date_readable < $twenty_four_hours_later ]]; then
+        forecast_condition=$(echo "$forecast_24h_info" | jq -r ".list[$i].weather[0].main")
+        forecast_temperature_kelvin=$(echo "$forecast_24h_info" | jq -r ".list[$i].main.temp")
+        forecast_temperature_celsius=$(kelvin_to_celsius $forecast_temperature_kelvin)
+        weather_icon_code=$(echo "$forecast_24h_info" | jq -r ".list[$i].weather[0].icon")
+
+        icon_url="https://openweathermap.org/img/w/${weather_icon_code}.png"
+
+        json_data+=$(cat <<EOF
+          {
+            "temperature_celsius": ${forecast_temperature_celsius},
+            "condition": "${forecast_condition}",
+            "icon_code": "${weather_icon_code}",
+            "date_readable": "${forecast_date_readable:11:5}"
+          },
+EOF
+)
+    fi
+done
+
+json_data+="
+  ]
+}
+"
+
+[ -f "data.json" ] && rm "weather.json"
+
+echo "$json_data" > weather.json
+
+git add README.md weather.json
+git commit -m "📄 Update latest data $(date +'%Y-%m-%d %H:%M:%S %Z')"
+git pull origin main
 git push origin main
